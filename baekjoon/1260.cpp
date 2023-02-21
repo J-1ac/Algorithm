ed2a7bc23d123879fdd44d37a7659c3eb408bab6
J-1ac <tkdwns26@naver.com>
2023-02-21 14:52:14 +0900

    commit

---
+++ baekjoon/1260.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void add_newvertex(int v1, int v2, vector<int> &v_list){
+    vector<int>::iterator p1 = find(v_list.begin(), v_list.end(), v1);
+    vector<int>::iterator p2 = find(v_list.begin(), v_list.end(), v2);
+    if(p1 == v_list.end()) v_list.push_back(v1);
+    if(p2 == v_list.end()) v_list.push_back(v2);
+}
+
+void add_newedge(int v1, int v2, vector<pair<int,int>> &e_list){
+    pair<int,int> newedge = make_pair(v1,v2);
+    e_list.push_back(newedge);
+}
+
+int main(){
+    int n, m, v, vertex1, vertex2;
+    vector<int> vertics; vector<pair<int,int>> edges;
+    cin >> n >> m >> v;
+    
+    for(int i=0; i<m; i++){
+        cin >> vertex1 >> vertex2;
+        add_newvertex(vertex1, vertex2, vertics);
+        add_newedge(vertex1, vertex2, edges);
+    }
+
+    for(auto w: vertics)
+        cout << w << " , ";
+    cout << '\n';    
+    for(auto w: edges)
+        cout << w.first << " , " << w.second << "\n";
+
+    return 0;
+}
