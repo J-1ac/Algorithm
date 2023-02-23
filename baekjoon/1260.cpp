f52bbf365a1fdfa99b6c4282789446fe8b63a474
SangJun Lee <tkdwns26@naver.com>
2023-02-23 22:40:24 +0900

    commit

--- baekjoon/1260.cpp
+++ baekjoon/1260.cpp
@@ -1,39 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> dfs_visited, dfs_result;
 
-void add_newvertex(int v1, int v2, vector<int> &v_list){
-    vector<int>::iterator p1 = find(v_list.begin(), v_list.end(), v1);
-    vector<int>::iterator p2 = find(v_list.begin(), v_list.end(), v2);
-    if(p1 == v_list.end()) v_list.push_back(v1);
-    if(p2 == v_list.end()) v_list.push_back(v2);
+void dfs(const vector<int> tree[], int node){
+    auto is_visited = find(dfs_visited.begin(), dfs_visited.end(), node);
+    if (is_visited==dfs_visited.end()){
+        dfs_visited.push_back(node);
+        dfs_result.push_back(node);
+    } else return;
+    for(int i=0; i<tree[node].size(); i++){
+        int next_node = tree[node][i];
+        dfs(tree, next_node);
+    }
 }
 
-void add_newedge(int v1, int v2, vector<pair<int,int>> &e_list){
-    pair<int,int> newedge = make_pair(v1,v2);
-    e_list.push_back(newedge);
-}
-
-void dfs(vector<int> &vertics, vector<pair<int,int>> &edges){
-
-}
-
-void bfs(vector<int> &vertics, vector<pair<int,int>> &edges){
+void bfs(const vector<int> tree[], int root){
 
 }
 
 int main(){
     int n, m, v, vertex1, vertex2;
-    vector<int> vertics; vector<pair<int,int>> edges;
     cin >> n >> m >> v;
+    vector<int> mytree[n+1];
     
     for(int i=0; i<m; i++){
         cin >> vertex1 >> vertex2;
-        add_newvertex(vertex1, vertex2, vertics);
-        add_newedge(vertex1, vertex2, edges);
+        mytree[vertex1].push_back(vertex2);
+        mytree[vertex2].push_back(vertex1);
     }
 
-    dfs(vertics, edges);
-    bfs(vertics, edges);
+    for(int i=1; i<=n; i++) sort(mytree[i].begin(), mytree[i].end());
+
+    dfs(mytree, v);
+    for (int v:dfs_result) cout << v << ' ';
+    cout << "\n";
+    bfs(mytree, v);
 
     return 0;
 }
