81c7fa6ddfdc400ad7fa461c05bae64ea4281122
SangJun Lee <tkdwns26@naver.com>
2023-03-07 17:04:13 +0900

    20230307

--- baekjoon/11729.cpp
+++ baekjoon/11729.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 vector<pair<int, int>> my_hanoi;
 
-void hanoi(int n){
-
+void hanoi(int n, int from, int by, int to){
+    if(n==0) return;
+    hanoi(n-1, from, to, by);
+    my_hanoi.push_back(make_pair(from, to));
+    hanoi(n-1, by, from, to);
 }
 
 int main(){
     int n;
     cin >> n;
  
-    hanoi(n);
+    hanoi(n, 1, 2, 3);
 
     cout << my_hanoi.size() << "\n";
     for(pair<int,int> phase: my_hanoi){
