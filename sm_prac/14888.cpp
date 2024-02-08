dd5c0412805f07430a532f8a349886131c6c7311
Sangjun Lee <tkdwns26@naver.com>
2024-02-08 17:23:51 +0900

    add sm_prac

---
+++ sm_prac/14888.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello";
+    // int n, num, cp, cs, cm, cd; vector<int> an;
+    // cin >> n;
+    // for(int i=0; i<n; i++){
+    //     cin >> num;
+    //     an.push_back(num);   
+    // }
+    // cin >> cp >> cs >> cm >> cd;
+
+    // for(int i=0; i<n; i++){
+    //     cout << an[i] << " ";   
+    // }
+
+    // cout << "\n" << cp << " " <<  cs << " " << " " <<  cm << " " << cd;
+    
+    // return 0;
+}
