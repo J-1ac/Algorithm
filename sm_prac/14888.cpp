1090fc57659136c20f7ed1ef359fde6d118b33ec
J-1ac <tkdwns26@naver.com>
2024-02-11 17:24:45 +0900

    commit test

--- sm_prac/14888.cpp
+++ sm_prac/14888.cpp
@@ -3,19 +3,22 @@
 using namespace std;
 
 int main(){
-    int n, num, cp, cs, cm, cd; vector<int> an;
-    cin >> n;
-    for(int i=0; i<n; i++){
-        cin >> num;
-        an.push_back(num);   
-    }
-    cin >> cp >> cs >> cm >> cd;
 
-    for(int i=0; i<n; i++){
-        cout << an[i] << " ";   
-    }
-
-    cout << "\n" << cp << " " <<  cs << " " <<  cm << " " << cd;
-    
+    cout << "Test";
     return 0;
+    // int n, num, cp, cs, cm, cd; vector<int> an;
+    // cin >> n;
+    // for(int i=0; i<n; i++){
+    //     cin >> num;
+    //     an.push_back(num);   
+    // }
+    // cin >> cp >> cs >> cm >> cd;
+
+    // for(int i=0; i<n; i++){
+    //     cout << an[i] << " ";   
+    // }
+
+    // cout << "\n" << cp << " " <<  cs << " " <<  cm << " " << cd;
+    
+    // return 0;
 }
