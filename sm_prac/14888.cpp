925f3a8c771aa16920d87aa779cd69e5fec47000
J-1ac <tkdwns26@naver.com>
2024-02-13 13:41:58 +0900

    add readme

--- sm_prac/14888.cpp
+++ sm_prac/14888.cpp
@@ -2,8 +2,12 @@
 #include <iostream>
 using namespace std;
 
+/* how to solve
+    
+*/
+
 int main(){
-    int n, num, cp, cs, cm, cd; vector<int> an;
+    int n, num, cp, cs, cm, cd, min = INT_MIN, max = INT_MAX; vector<int> an;
     cin >> n;
     for(int i=0; i<n; i++){
         cin >> num;
@@ -11,11 +15,8 @@ int main(){
     }
     cin >> cp >> cs >> cm >> cd;
 
-    for(int i=0; i<n; i++){
-        cout << an[i] << " ";   
-    }
+    cout << min << "\n" << max;
 
-    cout << "\n" << cp << " " <<  cs << " " <<  cm << " " << cd;
     
     return 0;
 }
