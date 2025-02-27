8554cb3720703d8459af0589971b8757fff74411
J-1ac <tkdwns26@naver.com>
2025-02-27 09:34:14 +0900

    1967

---
+++ baekjoon/java/boj1967/Main.java
@@ -0,0 +1,94 @@
+package boj1967;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Node {
+
+    int n;
+    int d;
+
+    public Node(int n, int d) {
+        this.n = n;
+        this.d = d;
+    }
+}
+
+class Edge {
+    int to;
+    int weight;
+
+    public Edge(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
+    }
+}
+
+public class Main {
+
+    public static int n;
+    public static int diameter;
+
+    private static int bfs(List<List<Edge>> graph, int start) {
+        int[] distances = new int[n + 1];
+        boolean[] visited = new boolean[n + 1];
+        Queue<Node> q = new LinkedList<>();
+
+        q.add(new Node(start, 0));
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            Node current = q.poll();
+            distances[current.n] = current.d;
+
+            for (Edge edge : graph.get(current.n)) {
+                int next = edge.to;
+                int weight = edge.weight;
+
+                if (!visited[next]) {
+                    visited[next] = true;
+                    q.add(new Node(next, current.d + weight));
+                }
+            }
+        }
+
+        int maxIndex = 1;
+        int maxDistance = distances[1];
+
+        for (int i = 2; i <= n; i++) {
+            if (distances[i] > maxDistance) {
+                maxIndex = i;
+                maxDistance = distances[i];
+            }
+        }
+
+        diameter = maxDistance;
+        return maxIndex;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        List<List<Edge>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n-1; i++) {
+            int node = sc.nextInt();
+            int connected = sc.nextInt();
+            int weight = sc.nextInt();
+            graph.get(node).add(new Edge(connected, weight));
+            graph.get(connected).add(new Edge(node, weight));
+        }
+
+        int farthestNode = bfs(graph, 1);
+        bfs(graph, farthestNode);
+
+        System.out.println(diameter);
+    }
+}
