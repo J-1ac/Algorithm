f7273cc072b5072f1b966a76da1d583db221b476
Lee <lee@Leeui-MacBookPro.local>
2025-03-05 14:21:18 +0900

    15663

--- baekjoon/test.cpp
+++ baekjoon/test.cpp
@@ -2,13 +2,7 @@
 using namespace std;
 
 int main(){
-    int d[15]{};
-
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            d[4*j]=i+j;
-        }
+    for(int i=0; i<10; i++){
+        cout << "hello, world!";
     }
-
-    for(int n :d) cout << n << '\t';
 }
