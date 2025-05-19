255a415c816f60c129ae9724ce29ede0e177746f
J-1ac <tkdwns26@naver.com>
2025-05-19 13:08:49 +0900

    test

---
+++ baekjoon/java/test/Main.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args){
+        HashSet<Integer> s = new HashSet<>();
+        HashMap<String, String> m = new HashMap<>();
+    }
+}
